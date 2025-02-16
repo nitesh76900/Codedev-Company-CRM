7ef1d444bb18e4158c359088959052613647dc75
Nitesh <niteshkumawat76900@gmail.com>
2025-02-16 09:27:16 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
@@ -19,31 +21,34 @@ app.use(
 app.get("/", (req, res) => {
   res.status(200).send("Setup backend");
 });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
-const authRoutes = require("./routes/auth.routes")
-const superAdminRoutes = require("./routes/superAdmin.routes")
-const companyRoutes = require("./routes/company.routes")
-const todoRoutes = require("./routes/todo.routes")
-const taskAssignedRoutes = require("./routes/taskAssigned.routes")
-const meetingRoutes = require("./routes/meeting.routes")
-const contactRoutes = require("./routes/contact.routes")
-const leadRoutes = require("./routes/lead.routes")
-const reminderRoutes = require("./routes/reminder.routes")
-const stickyNoteRoutes = require("./routes/stickyNote.routes")
-const calendarRoutes = require("./routes/calendar.routes")
-const dashboardRoutes = require("./routes/dashboard.routes")
+const authRoutes = require("./routes/auth.routes");
+const superAdminRoutes = require("./routes/superAdmin.routes");
+const companyRoutes = require("./routes/company.routes");
+const todoRoutes = require("./routes/todo.routes");
+const taskAssignedRoutes = require("./routes/taskAssigned.routes");
+const meetingRoutes = require("./routes/meeting.routes");
+const contactRoutes = require("./routes/contact.routes");
+const leadRoutes = require("./routes/lead.routes");
+const reminderRoutes = require("./routes/reminder.routes");
+const stickyNoteRoutes = require("./routes/stickyNote.routes");
+const calendarRoutes = require("./routes/calendar.routes");
+const dashboardRoutes = require("./routes/dashboard.routes");
 
-app.use("/api/v1/auth", authRoutes)
-app.use("/api/v1/super-admin", superAdminRoutes)
-app.use("/api/v1/company", companyRoutes)
-app.use("/api/v1/todo", todoRoutes)
-app.use("/api/v1/task-assigned", taskAssignedRoutes)
-app.use("/api/v1/meeting", meetingRoutes)
-app.use("/api/v1/contact", contactRoutes)
-app.use("/api/v1/lead", leadRoutes)
-app.use("/api/v1/reminder", reminderRoutes)
-app.use("/api/v1/sticky-note", stickyNoteRoutes)
-app.use("/api/v1/calender-data", calendarRoutes)
-app.use("/api/v1/dashboard-data", dashboardRoutes)
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/super-admin", superAdminRoutes);
+app.use("/api/v1/company", companyRoutes);
+app.use("/api/v1/todo", todoRoutes);
+app.use("/api/v1/task-assigned", taskAssignedRoutes);
+app.use("/api/v1/meeting", meetingRoutes);
+app.use("/api/v1/contact", contactRoutes);
+app.use("/api/v1/lead", leadRoutes);
+app.use("/api/v1/reminder", reminderRoutes);
+app.use("/api/v1/sticky-note", stickyNoteRoutes);
+app.use("/api/v1/calender-data", calendarRoutes);
+app.use("/api/v1/dashboard-data", dashboardRoutes);
 
 module.exports = app;
